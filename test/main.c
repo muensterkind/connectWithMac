3334fd1066a21c8cb79b89b6f7606c31ad7a44ef
정가현 <junggahyun@jeong-gahyeon-ui-MacBook-Pro.local>
2018-06-13 00:30:38 +0900

    first commit test

---
+++ test/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  test
+//
+//  Created by 정가현 on 2018. 6. 13..
+//  Copyright © 2018년 정가현. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
