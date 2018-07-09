fc5376bda973fca94fbe3398f4c43ca1d0e19d52
muensterkind <21700648@handong.edu>
2018-07-09 20:59:15 +0900

    180709 make com.eightcruz.javaStudy

---
+++ javaStudy/src/com/eightcruz/javaStudy/AccessObjExam.java
@@ -0,0 +1,15 @@
+package com.eightcruz.javaStudy;
+
+import javaStudy.AccessObj;
+
+public class AccessObjExam {
+
+	public static void main(String[] args) {
+		AccessObj obj = new AccessObj();
+		System.out.println(obj.p);
+		System.out.println(obj.p2);
+		System.out.println(obj.k);
+		System.out.println(obj.i);
+	}
+
+}
