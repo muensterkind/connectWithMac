aa0aca10074d6e1057f3d45e76fd676c35cd1069
muensterkind <21700648@handong.edu>
2018-07-09 21:43:10 +0900

    180709 edit Car.java

--- javaStudy/src/javaStudy/Car.java
+++ javaStudy/src/javaStudy/Car.java
@@ -2,7 +2,11 @@ package javaStudy;
 
 public class Car {
 	
-	public Car(String name) {
-		System.out.println("Car의 name 생성자입니다. ");
+//	public Car(String name) {
+//		System.out.println("Car의 name 생성자입니다. ");
+//	}
+	
+	public void run() {
+		System.out.println("Car의 run 메소드");
 	}
 }
