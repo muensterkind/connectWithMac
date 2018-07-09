17ac218a417331cf147f62580eb4e2afc82225b0
muensterkind <21700648@handong.edu>
2018-07-09 20:46:55 +0900

    180709 new BusExam.java

---
+++ javaStudy/src/javaStudy/BusExam.java
@@ -0,0 +1,16 @@
+package javaStudy;
+
+public class BusExam {
+
+	public static void main(String[] args) {
+		Bus bus = new Bus();
+		bus.run();
+		bus.ppangppang();
+		
+		Car car = new Car();
+		car.run();
+		
+		//car.ppangppang();
+	}
+
+}
