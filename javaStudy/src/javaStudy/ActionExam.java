07949f47f02e19a80c33f074e3f3119ec157347c
muensterkind <21700648@handong.edu>
2018-07-10 01:38:28 +0900

    180710 new ActionExam.java

---
+++ javaStudy/src/javaStudy/ActionExam.java
@@ -0,0 +1,19 @@
+package javaStudy;
+
+public class ActionExam {
+
+	public static void main(String[] args) {
+//		Action action = new MyAction();
+//		action.exec();
+		
+		Action action = new Action() {
+			
+			@Override
+			public void exec() {
+				System.out.println("exec");
+			}
+		};
+		action.exec();
+	}
+
+}
