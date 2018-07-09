4cc50a53e83b80fe0bc0714491fe1a0cd2e5a875
muensterkind <21700648@handong.edu>
2018-07-09 21:42:34 +0900

    180709 edit BusExam.java

--- javaStudy/src/javaStudy/BusExam.java
+++ javaStudy/src/javaStudy/BusExam.java
@@ -5,12 +5,6 @@ public class BusExam {
 	public static void main(String[] args) {
 		Bus bus = new Bus();
 		bus.run();
-		bus.ppangppang();
-		
-		Car car = new Car();
-		car.run();
-		
-		//car.ppangppang();
 	}
 
 }
