9064592c8c244605d5462391eb555eecfdc5798d
muensterkind <21700648@handong.edu>
2018-07-09 21:00:00 +0900

    180709 new AccessObjExam.java

--- javaStudy/src/com/eightcruz/javaStudy/AccessObjExam.java
+++ javaStudy/src/com/eightcruz/javaStudy/AccessObjExam.java
@@ -2,14 +2,14 @@ package com.eightcruz.javaStudy;
 
 import javaStudy.AccessObj;
 
-public class AccessObjExam {
+public class AccessObjExam extends AccessObj {
 
 	public static void main(String[] args) {
-		AccessObj obj = new AccessObj();
+		AccessObjExam obj = new AccessObjExam();
 		System.out.println(obj.p);
 		System.out.println(obj.p2);
-		System.out.println(obj.k);
-		System.out.println(obj.i);
+		//System.out.println(obj.k);
+		//System.out.println(obj.i);
 	}
 
 }
