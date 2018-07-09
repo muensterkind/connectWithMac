faf273eb04b1f6bfbcf94fcc7a8f1982850522f5
muensterkind <21700648@handong.edu>
2018-07-09 21:42:09 +0900

    180709 edit Bus.java

--- javaStudy/src/javaStudy/Bus.java
+++ javaStudy/src/javaStudy/Bus.java
@@ -1,7 +1,8 @@
 package javaStudy;
 
 public class Bus extends Car {
-	public void ppangppang() {
-		System.out.println("빵빵");
+	public void run() {
+		super.run();
+		System.out.println("Bus의 run 메소드");
 	}
 }
