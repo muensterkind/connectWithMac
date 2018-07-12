78e34d7f390a9837ff7f73f31ed1ec53c171b2a8
muensterkind <21700648@handong.edu>
2018-07-12 23:38:33 +0900

    180712 edit TruckExam.java one more

--- javaStudy/src/javaStudy/TruckExam.java
+++ javaStudy/src/javaStudy/TruckExam.java
@@ -3,7 +3,7 @@ package javaStudy;
 public class TruckExam {
 
 	public static void main(String[] args) {
-		Truck t = new Truck(); // 보여주기
+		Truck t = new Truck(); // 보여주기용
 		
 	//	Car c = new Car();
 	}
