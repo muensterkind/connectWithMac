9f63d42a609150ede90cf662bce7ff384ac11c8b
muensterkind <21700648@handong.edu>
2018-07-14 22:45:28 +0900

    180714 edit AccessObjExam.java

--- javaStudy/src/com/eightcruz/javaStudy/AccessObjExam.java
+++ javaStudy/src/com/eightcruz/javaStudy/AccessObjExam.java
@@ -8,7 +8,7 @@ public class AccessObjExam extends AccessObj {
 		AccessObjExam obj = new AccessObjExam();
 		System.out.println(obj.p);
 		System.out.println(obj.p2);
-		//System.out.println(obj.k);
+		//System.out.println(obj.k); // 보여주기
 		//System.out.println(obj.i);
 	}
 
