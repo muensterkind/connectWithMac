fd1b3fb78d423f8325aa2afb69d890da44fd9a61
muensterkind <21700648@handong.edu>
2018-08-13 21:29:44 +0900

    create Problem01-1-4.cpp

--- C++Study/Problem01-1-4.cpp
+++ C++Study/Problem01-1-4.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
-int CalSalary(int sales)
-{
+int CalSalary(int sales) {
   return (int)(50 + sales * 0.12);
 }
 
-int main(void)
-{
+int main(void) {
   int sales;
-  while(1)
-  {
+  while(1) {
     std::cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
     std::cin >> sales;
     if(sales == -1)
